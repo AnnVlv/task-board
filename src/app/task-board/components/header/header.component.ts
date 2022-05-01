96423bcfdbb87d385a99987d5d5e7e7e9a1a5445
AnnVlv <anna.valyaeva17@gmail.com>
2022-05-01 19:55:26 +0300

    show current time

--- src/app/task-board/components/header/header.component.ts
+++ src/app/task-board/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject, takeUntil } from 'rxjs';
 
 import { AuthService } from '../../../core/services';
 
@@ -7,11 +8,29 @@ import { AuthService } from '../../../core/services';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  public date$ = new Subject<Date>();
+
+  private destroy$ = new Subject<void>();
+
   public get username(): string {
     return this.authService.user?.username || '';
   }
+
   constructor(
     private readonly authService: AuthService,
   ) { }
+
+  public ngOnInit(): void {
+    interval(60).pipe(
+      takeUntil(this.destroy$),
+    ).subscribe(() => {
+      this.date$.next(new Date());
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
