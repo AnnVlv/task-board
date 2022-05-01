9cfc9006a9a0a90437c1ce613a6dc0fd7ffdfa45
AnnVlv <anna.valyaeva17@gmail.com>
2022-05-01 18:39:49 +0300

    feature add list

--- src/app/task-board/components/board/board.component.ts
+++ src/app/task-board/components/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
 import { LISTS } from 'src/app/data/lists';
 
 @Component({
@@ -9,8 +11,29 @@ import { LISTS } from 'src/app/data/lists';
 export class BoardComponent implements OnInit {
   public LISTS = LISTS;
 
+  public addListDialogVisible = false;
+
+  public newListTitle = new FormControl('', [Validators.required]);
+
   constructor() { }
 
   public ngOnInit(): void {
+  }
+
+  public openAddListDialog(): void {
+    this.addListDialogVisible = true;
+  }
+
+  public closeAddListDialog(canceled: boolean): void {
+    if (!canceled) {
+      this.LISTS.push({
+        id: this.LISTS.length + 1,
+        title: this.newListTitle.value,
+        items: [],
+      });
+    }
+
+    this.newListTitle.reset();
+    this.addListDialogVisible = false;
   }
 }
--- src/app/task-board/task-board.module.ts
+++ src/app/task-board/task-board.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { TaskBoardRoutingModule } from './task-board-routing.module';
 import { TaskBoardComponent } from './task-board.component';
@@ -19,6 +23,9 @@ import { BoardComponent } from './components/board/board.component';
     TaskBoardRoutingModule,
     ButtonModule,
     ToolbarModule,
+    DialogModule,
+    ReactiveFormsModule,
+    InputTextModule,
   ]
 })
 export class TaskBoardModule { }
